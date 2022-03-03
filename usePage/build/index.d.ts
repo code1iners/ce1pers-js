c49dabeba5a9889e5d5a1e893ebfb63eee1e9b62
Codeliner <codeliner96@gmail.com>
2022-03-03 22:33:20 +0900

    Export types from root source.

--- usePage/build/index.d.ts
+++ usePage/build/index.d.ts
@@ -1,2 +1,3 @@
 export { generator } from "./src/usePage";
+export { IGenerateProps, IGeneratePageData, IGenerateResult, } from "./src/types/usePage";
 export { testData } from "./src/data/testData";
--- usePage/index.ts
+++ usePage/index.ts
@@ -1,2 +1,7 @@
 export { generator } from "./src/usePage";
+export {
+  IGenerateProps,
+  IGeneratePageData,
+  IGenerateResult,
+} from "./src/types/usePage";
 export { testData } from "./src/data/testData";
