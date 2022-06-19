610b112c5fd3aba5613177b8d63efbc34cdacb7d
Codeliner <codeliner96@gmail.com>
2022-06-19 17:20:03 +0900

    [v.1.2.0] Added window resize event listener.

--- useWindow/src/useScreen.ts
+++ useWindow/src/useScreen.ts
@@ -10,13 +10,25 @@ interface WindowSize {
  */
 export const useScreen = () => {
   const [windowSize, setWindowSize] = useState<WindowSize>();
+
+  const onResize = () =>
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+
   useEffect(() => {
     if (window) {
       setWindowSize({
         width: window.innerWidth,
         height: window.innerHeight,
       });
+
+      window.addEventListener("resize", onResize);
     }
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
   }, []);
 
   return {
